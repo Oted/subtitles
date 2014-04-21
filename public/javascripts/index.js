871c3e5dc5df3f0479fdb173b4a980ef49c672b7
Oted <herr.olle@gmail.com>
2014-04-21 22:30:18 +0200

    minor UI changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,4 @@
+var i = 0;
 $(document).ready(function(){
     $("#sub_title").bind('keyup', function(event){ 
         if(event.keyCode === 13){ 
@@ -14,6 +15,7 @@ var getSub = function(){
     $.ajax({
         url: "/get/?language="+ $("#language").val() +"&title="+title.replace(/_/g,"."),
         cache: false,
+        timeout: 1000,
         beforeSend: function(){
             $("#result").hide();
             document.getElementById("result").innerHTML = "";
@@ -42,7 +44,10 @@ var getSub = function(){
             $("#result").removeClass("btn-success");
             $("#result").addClass("btn-danger");
             document.getElementById("result").href = "#";
-            if ($("#language").val() === "English"){
+            document.getElementById("sub_title").value = "";
+            if (i++ > 2){
+                document.getElementById("result").innerHTML = "Nothing could be found, sorry"; 
+            } else if ($("#language").val() === "English"){
                 document.getElementById("result").innerHTML = "Nothing found, maybe you need to be more specfic?";
             } else {
                 document.getElementById("result").innerHTML = "No subtitle could be found, try selecting English instead!";
