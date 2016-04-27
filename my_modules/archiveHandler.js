f1f8a8587478e9c48c03b7f989a59d996c2efff4
Oted <herr.olle@gmail.com>
2016-04-27 23:37:40 +0200

    attempt 1

--- my_modules/archiveHandler.js
+++ my_modules/archiveHandler.js
@@ -1,27 +1,32 @@
 var http = require("http"),
-    fs = require("fs");
+    fs = require("fs"),
+    request = require('request'),
+    AdmZip = require('adm-zip');
 
 /**
- *  get the rarfile of the best result and extracts the srt file  
+ *  get the rarfile of the best result and extracts the srt file
  */
 exports.extractFile = function(url, language, callback){
-    var file;
-    var request = http.get(url, function(res) {
+
+    return request({'uri' : url, 'headers' : {
+        'accept-encodinfg' : 'gzip, deflate, sdch',
+        'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'
+    }}, function(body, res, err) {
+        var file;
         if (!res || !res.headers || !res.headers["content-disposition"]){
             callback("");
             console.log("------- error: res was not defined ---------");
             return;
         }
-        
+
         var resFileType = res.headers["content-disposition"].split(".").pop(),
             archiveFileName = res.headers["content-disposition"].split("=").pop(),
-            uniqueName = new Date().getTime(); 
-        
+            uniqueName = new Date().getTime();
+
         file = fs.createWriteStream("./archives/" + archiveFileName);
         res.pipe(file);
         file.on("finish", function() {
-            res.emit("end");
-            request.end();
             if (archiveFileName){
                 getArchiveContent(archiveFileName, uniqueName, language, function(fileName){
                     callback(fileName);
@@ -51,7 +56,7 @@ var getArchiveContent = function(archiveFileName, uniqueName, language, callback
             } else {
                 checkContent(archiveFileName, uniqueName, language, callback);
             }
-        }); 
+        });
     } else if (fileType === "rar"){
         child = exec("unrar e " + "./archives/" + archiveFileName + " ./archives/" + uniqueName + "/",
                 function (error, stdout, stderr) {
@@ -61,7 +66,7 @@ var getArchiveContent = function(archiveFileName, uniqueName, language, callback
             } else {
                 checkContent(archiveFileName, uniqueName, language, callback);
             }
-        }); 
+        });
     } else {
         callback("");
     };
@@ -72,7 +77,7 @@ var checkContent = function(archiveFileName, uniqueName, language, callback){
         fileType = archiveFileName.split(".").pop(),
         //do this
         archiveName = archiveFileName.substring(0, archiveFileName.lastIndexOf("."));
-    
+
     fs.readdir("./archives/"+uniqueName+"/", function(err, files){
         files.forEach(function(file){
             if (isSubtitle(file)){
@@ -83,7 +88,7 @@ var checkContent = function(archiveFileName, uniqueName, language, callback){
             fs.rename("./archives/"+uniqueName+"/"+subtitles[0], "./output/"+ language + "_" + subtitles[0], function(){
                 callback(language + "_" + subtitles[0]);
                 removeArchiveFiles(archiveFileName, uniqueName);
-            });    
+            });
         } else if (subtitles.length > 1){
             fs.rename("./archives/" + archiveFileName, "./output/" + language + "_" + archiveFileName, function(){
                 callback(language + "_" + archiveFileName);
@@ -93,7 +98,7 @@ var checkContent = function(archiveFileName, uniqueName, language, callback){
             console.log("Archive seems to be empty");
             removeArchiveFiles(archiveFileName, uniqueName);
             callback("");
-        } 
+        }
     });
 };
 
@@ -103,14 +108,14 @@ var removeArchiveFiles = function(archiveFileName, uniqueName){
         exec = require('child_process').exec,
         child;
 
-    child = exec("rm -rf " + dir, function (error, stdout, stderr){}); 
-    child = exec("rm " + archive ,function (error, stdout, stderr) {}); 
+    child = exec("rm -rf " + dir, function (error, stdout, stderr){});
+    child = exec("rm " + archive ,function (error, stdout, stderr) {});
 };
 
 var isSubtitle = function(fileName){
     file = fileName.split(".").pop();
     var postFix = ["txt", "srt", "ass", "ssa", "sub", "jss", "gsub", "usf", "idx"];
-    
+
     for (var i = postFix.length - 1; i >=0; i--){
         if (file === postFix[i]){
             return true;
--- my_modules/crawler.js
+++ my_modules/crawler.js
@@ -6,7 +6,6 @@ var util            = require("./util.js"),
     cheerio         = require('cheerio'),
     fs              = require("fs");
 
-
 /**
  * Checks if this search has been cached before
  */
@@ -23,14 +22,14 @@ exports.checkCache = function(query, language, callback) {
         child;
 
     if (cache.length === 0){
-        callback("");
-        return;
+        return callback("");
     }
+
     for (var i = cache.length - 1; i >= 0; i--){
         var entity = cache[i];
 
         if (!fs.existsSync("./output/" + entity.getValue()) || entity.hasExpired()) {
-            child = exec("rm ./output/" + entity.getValue(), function (error, stdout, stderr){}); 
+            child = exec("rm ./output/" + entity.getValue(), function (error, stdout, stderr){});
             cache.splice(i, 1);
             console.log(entity.getValue() + " has been removed from cache.");
         } else if (entity.equals(query, language)){
@@ -51,71 +50,74 @@ exports.getData = function(target, query, targetLanguage, res) {
     var url = target + query,
         bestPossible = [],
         worstPossible = [];
-   
+
     console.log("Request received : " + url);
     request(url, function (error, response, html) {
         if (error){
-            return ;
+            res.send(404, "Sorry, nothing could be found, maybe you need to be more specific?");
         }
-        
+
         var $ = cheerio.load(html),
             //minimum level for levenstein, if no result is below this the quaility will
-            //be set to 0 and the final result will show up in yellow. 
+            //be set to 0 and the final result will show up in yellow.
             levenDistanceMin = 0.28;
-    
-        $(".a1").each(function(i, data){
+
+        $(".a1").each(function(i, data) {
             var language = $($(data).find("span")[0]).text().toLowerCase(),
                 title = $($(data).find("span")[1]).text().toLowerCase(),
                 url = "http://subscene.com" + $($(data).find("a")[0]).attr("href"),
                 newDistance = util.getTotalLeven(query, title);
-    
+
             if (language.indexOf(targetLanguage) >= 0 && newDistance < levenDistanceMin) {
                 levenDistanceMin = newDistance;
                 bestPossible = [];
                 bestPossible.push(url);
-            } else if (language.indexOf(targetLanguage) >= 0 && newDistance === levenDistanceMin){
-                if (bestPossible.indexOf(url) < 0) bestPossible.push(url);  
+            } else if (language.indexOf(targetLanguage) >= 0 && newDistance === levenDistanceMin) {
+                if (bestPossible.indexOf(url) < 0) bestPossible.push(url);
             } else if (language.indexOf(targetLanguage) >= 0){
-                if (worstPossible.indexOf(url) < 0) worstPossible.push(url);  
+                if (worstPossible.indexOf(url) < 0) worstPossible.push(url);
             }
         });
-        
+
         if (worstPossible.length < 1 && bestPossible.length < 1) {
-            res.send(404, "Sorry, nothing could be found, maybe you need to be more specific?");
-        } else if (bestPossible.length > 0){
-            getBestUrls(bestPossible, function(best){
-                if (best.url.length > 0){
-                    archivehandler.extractFile(best.url, targetLanguage, function(fileName){
-                        if (fileName){
+            return res.send(404, "Sorry, nothing could be found, maybe you need to be more specific?");
+        }
+
+        if (bestPossible.length > 0) {
+            return getBestUrls(bestPossible, function(best) {
+
+                if (best.url.length > 0) {
+                    archivehandler.extractFile(best.url, targetLanguage, function(fileName) {
+                        if (fileName) {
                             console.log("Responded with good quailty " + fileName + ", new cache has been created.");
                             res.json({"filename" : fileName, "quality":"1"});
-                            cache.push(new CacheEntity(query, fileName, targetLanguage));   
-                            res.end();
+                            cache.push(new CacheEntity(query, fileName, targetLanguage));
+                            return res.end();
                         } else {
-                            res.send(404, "Sorry, nothing could be found :(");
+                            return res.send(404, "Sorry, nothing could be found :(");
                         }
                     });
                 } else {
-                    res.send(404, "Sorry, nothing could be found :(");
+                    return res.send(404, "Sorry, nothing could be found :(");
                 };
             });
-        } else {
-            getBestUrls(worstPossible, function(best){
-                if (best.url.length > 0){
-                    archivehandler.extractFile(best.url, targetLanguage, function(fileName){
-                        if (fileName){
-                            console.log("Responded with bad quailty " + fileName);
-                            res.json({"filename" : fileName, "quality":"0"});
-                            res.end();
-                        } else {
-                            res.send(404, "Sorry, nothing could be found :(");
-                        }
-                    });
-                } else {
-                    res.send(404, "Sorry, nothing could be found :(");
-                }
-            });
-        };
+        }
+
+        return getBestUrls(worstPossible, function(best) {
+            if (best.url.length > 0) {
+                archivehandler.extractFile(best.url, targetLanguage, function(fileName) {
+                    if (fileName) {
+                        console.log("Responded with bad quailty " + fileName);
+                        res.json({"filename" : fileName, "quality":"0"});
+                        res.end();
+                    } else {
+                        res.send(404, "Sorry, nothing could be found :(");
+                    }
+                });
+            } else {
+                res.send(404, "Sorry, nothing could be found :(");
+            }
+        });
     });
 };
 
@@ -134,7 +136,7 @@ var getBestUrls = function(urls, callbackhell){
                 callbackhell(best);
                 return ;
             }
-            
+
             var $ = cheerio.load(html);
             $("#details li").each(function(i, data){
                 var liContent = $(data).text().toLowerCase();
@@ -142,13 +144,13 @@ var getBestUrls = function(urls, callbackhell){
 
                     waiting--;
                     var downloads = parseInt(liContent.replace(/\D+/g, ""), 10);
-                     
+
                     if (best.score < downloads){
                         best.score = downloads;
                         best.url = "http://subscene.com" + $("#downloadButton").attr("href");
                     }
-                    
-                    if (waiting < 1){ 
+
+                    if (waiting < 1){
                         callbackhell(best);
                     }
                 } else {
